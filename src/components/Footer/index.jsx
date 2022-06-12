e1c549747737054798a7b3b52554289c563b72ce
pedrocleal <pedroxleal10@gmail.com>
2022-06-12 18:24:58 -0300

    feat: add scroll button on footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from "./styles";
+import { ArrowUp } from 'phosphor-react';
+
+import { useContext } from 'react';
+import { Container } from './styles';
+
+import { ThemeContext } from '../../App';
 
 export function Footer() {
+  const { theme } = useContext(ThemeContext);
+
+  function handleScrollToTop() {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }
+
   return (
-    <Container>
+    <Container theme={theme}>
       <p>©️ 2022 - Desenvolvido por Pedro Leal</p>
+      <button type="button" onClick={handleScrollToTop}>
+        <ArrowUp size={24} />
+      </button>
     </Container>
-  )
+  );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,6 +5,25 @@ export const Container = styled.footer`
   width: 100%;
   margin: 52px auto;
 
+  font-weight: 500;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    padding: 4px;
+    color: ${({ theme }) => (theme === 'dark' ? '#fff' : '#000')};
+    border-radius: 10px;
+    border: 2px solid #5200ff;
+    background: transparent;
+    transition: all 0.3s ease-in;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
   @media (max-width: 1500px) {
     max-width: 1080px;
   }
